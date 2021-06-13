8d7d3101b17e8b4668e0d365317261d242921aac
DaviLuisSantos <costasantos.daviluis@gmail.com>
2021-06-13 17:12:37 -0300

    Update with addresses

--- Src/controllers/UserController.js
+++ Src/controllers/UserController.js
@@ -1,3 +1,4 @@
+const { update } = require('../models/User');
 const User = require('../models/User');
 
 module.exports = {
@@ -11,6 +12,13 @@ module.exports = {
         const { name, email } = req.body;
 
         const user = await User.create({ name, email });
+
+        return res.json(user);
+    },
+    async update(req, res) {
+        const { name, email } = req.params;
+
+        const user = await User.update({ name, email });
 
         return res.json(user);
     }
